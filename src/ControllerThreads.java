841a357aa9643d6136af494430284d69d5e7067e
ZacharyC321 <35279655+ZacharyC321@users.noreply.github.com>
2018-02-12 21:46:30 -0800

    Added more to menu. moved Parfait's code over.
    I didn't delete your original code Parfait

--- src/ControllerThreads.java
+++ src/ControllerThreads.java
@@ -1,37 +1,136 @@
-
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Scanner;
 
 // ControllerThreads class:
 // Contains 2 threads, one for looping through the menu,
-// and one for checking if any reminders will go off.
-public class ControllerThreads extends Thread
+// and one for checking if any reminders/alarms will go off.
+public class ControllerThreads
 {
 	
-
-	public final static int TIME_BETWEEN_CHECKING_REMINDERS = 5000; // 30 seconds
+	// ADAM, feel free to change this as needed ...
+	public final static int TIME_BETWEEN_CHECKING_REMINDERS = 30000; // 30 seconds
 	
 	// Zac
 	public static class MenuThread extends Thread
 	{
 		
-		public View view;
-		public MenuThread()
-		{
-			this.view = new View();
-		}
+		public static View view = new View();
+		
+		public MenuThread() {}
 		
 		public void run()
 		{
+			// temporary
+			Scanner kb = new Scanner(System.in);
+			
 			while(true)
 			{
 				// menu loop
-				int input = view.menu();
-				if (input == 3)
+				menu();
+				String choice = kb.nextLine();//view.getInput();
+				switch (choice)
 				{
-					System.exit(0);
+				case "1": ViewReminders(); break;
+				case "2": CreateReminder(); break;
+				case "3": /*Model.saveReminders();*/ System.exit(0);
+				default: view.showMessage("Invalid input");
 				}
 			}
 		}
+		
+		// displays list of existing reminders
+		public void ViewReminders()
+		{
+			// get list from model and display to console through the view
+			
+			// user selects a reminder to edit/delete.
+			// selection made by typing name (?) or number...
+			
+			// edit reminder
+		}
+		
+		// deletes a reminder
+		private void DeleteReminder(Alarm al)
+		{
+			//Model.deleteReminder(al);
+			view.showMessage("Reminder deleted.");
+		}
+		
+		// EditReminder allows user to change any aspect of a reminder
+		// name, date, time, etc.
+		// the user can also delete the reminder.
+		private void EditReminder(Alarm al)
+		{
+			
+		}
+		
+		// CreateReminder creates a new reminder based on the user's specifications
+		// It adds the reminder to the reminder list and saves the reminder list to a file. 
+		private void CreateReminder()
+		{
+			String name ="", date = "", time = "";
+			LocalDate d = LocalDate.now();
+			LocalTime t = LocalTime.now();
+			boolean quit = false;
+			
+			// temporary
+			Scanner kb = new Scanner(System.in);
+			
+			view.showMessage("What is the reminder for? Short description: ");
+			name = kb.nextLine();
+			
+			
+			// get date and time for reminder
+			do {
+				if(quit)
+					break;
+				view.showMessage("Enter the date in format YYYY-MM-DD: ");
+				date = kb.nextLine();
+				try {
+					d = LocalDate.parse(date);
+					quit = true;
+				}catch(Exception e) {
+					view.showMessage("Invalid format...");
+				}
+			} while(!quit);
+			
+			quit = false;
+			do {
+				if(quit)
+					break;
+				view.showMessage("Enter the time in format HH:MM (24hr): ");
+				time = kb.nextLine();
+				try {
+					t = LocalTime.parse(time);
+					quit = true;
+				}catch (Exception e) {
+					view.showMessage("Invalid format...");
+				}
+				
+			} while(!quit);
+			
+			Alarm al = new Alarm(name, d, t);
+			view.showMessage("Reminder successfully created!");
+			
+			// add reminder to list
+			//Model.addReminder(al);
+			//Model.saveReminders();
+			
+		}
+		
+		
+		// print menu format
+		private void menu() 
+		{
+			view.showMessage("\n\t\tMenu\n"
+					+ "1. View existing reminders\n"
+					+ "2. Create a new reminder\n"
+					+ "3. Exit\nMake a selection:");
+		}
 	}
+	
+	
 	
 	
 	
@@ -48,13 +147,13 @@ public class ControllerThreads extends Thread
 				// check reminders from model
 				
 				//test
-					System.out.println("test");
+				//System.out.println("test");
 				//
 				
 				
 				try
 				{
-					// wait...
+					// wait 30 seconds
 					Thread.sleep(TIME_BETWEEN_CHECKING_REMINDERS);
 					
 				} catch (InterruptedException e)
@@ -68,12 +167,11 @@ public class ControllerThreads extends Thread
 	
 
 	
-	
-	
 	public static void main(String[] args)
 	{
 		new MenuThread().start();
 		new CheckRemindersThread().start();
+		
 	} 
 
 
