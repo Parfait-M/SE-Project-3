5c0660ac23f8ed46a4d3c79544c4a460bdf2a5c6
Adam <ADAM594@Adams-MacBook-Pro.local>
2018-02-14 11:57:56 -0800

    remove src

--- src/DataType.java
+++
@@ -1,7 +0,0 @@
-package src;
-
-public enum DataType {
-	INT,	// represent generic integers
-	STRING, // represent string data that can contain a white space
-	FLOAT;	// represents floating point numbers
-}
